f3a02a8a397b62c4f4d68baacd5528f97c9a8a94
aarongreenlee <aarongreenlee@gmail.com>
2022-12-21 11:30:56 -0500

    Setup some nvim plugins, updated keymaps

--- nvim/lua/aarongreenlee/core/keymaps.lua
+++ nvim/lua/aarongreenlee/core/keymaps.lua
@@ -1,4 +1,6 @@
-m.g.mapleader = " "
+
+-- set the leader key to "spacebar"
+vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 
 -- general
@@ -9,10 +11,11 @@ keymap.set("n", "<leader>+", "<C-a>") -- incrmeent number
 keymap.set("n", "<leader>-", "<C-x>") -- decrement number
 
 -- windows
-keymap.set("n", "<leader>sv", "<C-w>v") -- split vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split horizontally
+keymap.set("n", "<leader>s|", "<C-w>v") -- split vertically
+keymap.set("n", "<leader>s-", "<C-w>s") -- split horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- resize to equalize width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split
+
 -- tabs
 keymap.set("n", "<leader>to", ":tabnew<CR>") -- open a new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
@@ -21,8 +24,36 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to the previous tab
 
 -- plugins
 
+-- Move between windows using the vim-tmux-navigator plugin:
+-- leader, C-h: move window focus left
+-- leader, C-j: move window focus down
+-- leader, C-k: move window focus up
+-- leader, C-l: move window" focus right
+
+-- maximize windows: toggle window maximization
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
+-- vim-surround: surround text with characters, swap characters, etc.
+  -- To surround with a character (e.g., ") y,s + motion + :character
+  -- To delete a surrounded charater d,s + motion + :character
+  -- To change a surrounded character c,s + motion + :replace + replacedBy
+
+-- replace text with register
+  -- y,w to copy a word and then g,r,w to replace a word
+
+-- commenting plugin
+  -- g,c,c to comment selected text
+
+-- nvim-tree
+keymap.set("n", "<leader>12", ":NvimTreeToggle<CR>")
+keymap.set("n", "<leader>11", ":NvimTreeFocus<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
 
 
 
--- setup.sh
+++ setup.sh
@@ -23,8 +23,9 @@ echo "done"
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
+    echo "  Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+    echo ""
 done
 
 # install antigen zsh plugin manager
